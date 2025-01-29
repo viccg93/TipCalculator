a3b2e4fc38077fb67893432e9fe661fe33eda4d7
Vic CG <vic.cg93@gmail.com>
2025-01-28 18:44:21 -0600

    mod: se hace hoisting del state y se implementa calculo de propina fija al 15%

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -34,7 +34,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -66,6 +65,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TipTimeLayout() {
+    //siempre que se modifique amountInput va a disparar una recomposicion
+    var amountInput by remember { mutableStateOf("") }
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount)
+    //se puede usar la lambda quese definio dentro de EditNumberField ya que esya solo le pasa
+    //el nuevo valor a amountInput, lo que desencadena la recomposición
+    val onAmountChange: (String) -> Unit = {
+            newAmount -> amountInput = newAmount
+    }
     Column(
         modifier = Modifier
             .statusBarsPadding()
@@ -81,17 +89,32 @@ fun TipTimeLayout() {
                 .align(alignment = Alignment.Start)
         )
         //campo de texto
-        EditNumberField(modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
+        EditNumberField(
+            value = amountInput,
+            //usando la lambda
+            //onValueChange = onAmountChange,
+            onValueChange = {amountInput = it},
+            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth()
+        )
         Text(
-            text = stringResource(R.string.tip_amount, "$0.00"),
+            text = stringResource(R.string.tip_amount, tip),
             style = MaterialTheme.typography.displaySmall
         )
         Spacer(modifier = Modifier.height(150.dp))
     }
 }
 
+//para hacer hoisting al state de un composable se deben de agregar los parametros
+//value: T y onValueChange: (T) -> Unit
+//despues del hoisting este composable ya no tiene logica de estado y toda la logica
+//para convertir y calcular la propina se extrae para que sea accesible a otros composables
+//fuera de este contexto
 @Composable
-fun EditNumberField(modifier: Modifier = Modifier){
+fun EditNumberField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+){
     //mutableStateOf() retorna un valor que es considerado estado de la app
     //es mutable y es observable, por lo que cualquier cambio va a disparar una recomposition
     //la siguiente manera de usar un motableStateOf() marca el arbol de cambios como dirty
@@ -101,7 +124,22 @@ fun EditNumberField(modifier: Modifier = Modifier){
     //por lo que se dispara una nueva recomposition
     //se delega el getter y setter default a remember, por lo que ya no es necesario acceder directamente
     //a la propiedad value de MutableState
-    var amountInput by remember { mutableStateOf("") }
+    //cuando se hace hoisting la siguiente linea ya no tiene uso en este contexto
+    //var amountInput by remember { mutableStateOf("") }
+    //convertimos el input un valor double y se alamacena en amount
+    //en caso de ser null (la conversion no es posible) mediante el operador Elvis pasamos el valor de 0.0
+    //las siguientes variables tampoco tienen mas uso en este contexto
+    /*
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount)
+    */
+    //creamos una lambda que se va a pasar a onValueChange
+    //posterior al hoisting esta lambda se saca de este composable
+    /*
+    val CalculateAndDisplayTip: (String) -> Unit = {
+            newAmount -> amountInput = newAmount
+    }
+    */
     //se puede obtener el valor mediante la propiedad value cuando se usa mutableStateOf()
     TextField(
         //se agrega una etiqueta dentro del textField
@@ -115,10 +153,18 @@ fun EditNumberField(modifier: Modifier = Modifier){
         //value = amountInput.value
         //cuando se delega amountInput a remember, este es ya no es directamente un mutableStateOf
         //por lo que no se usa la propiedad value
-        value = amountInput,
+        //value = amountInput,
+        //posterior al hoisting, value se va a encadenar con el parametro que tambien se llama value
+        value = value,
         //esta asignacion dispara la recomposición de este composable ya que el value de amountInput se modifica
         //y aqui es donde se lee el valor de amountImput cuando se asigna al value de TextField
-        onValueChange = {newAmount -> amountInput = newAmount},
+        //onValueChange = CalculateAndDisplayTip,
+        //posterior al hoisting, tambien se va a encadenar el cambio de valor con la funcion externa
+        //esto se debe a que el estado ya no es interno a este Composable
+        //es decir ya no existe una variable amountInput que mantenga el estado, se maneja externamente
+        //mediante value y onValueChange
+        //al no tener estado, en este conxto no hay nada que actualizar, se reliza desde el exterior con la lambda
+        onValueChange = onValueChange,
         modifier = modifier,
         //condensa el TextField en una sola linea, horizontally scrollable
         singleLine = true,
