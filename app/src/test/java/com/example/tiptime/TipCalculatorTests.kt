9b502f48c5e342da9ba0eb6ebf3cd86531d16660
Vic CG <vic.cg93@gmail.com>
2025-01-29 13:03:43 -0600

    mod: se agrega local test de calculateTip

--- app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
+++ app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
@@ -1,4 +1,16 @@
 package com.example.tiptime
 
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.text.NumberFormat
+
 class TipCalculatorTests {
+    @Test
+    fun calculateTip_20percentNoRoundup() {
+        val amount = 100.00
+        val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(20.00)
+        val actualTip = calculateTip(amount, tipPercent, false)
+        assertEquals(expectedTip, actualTip)
+    }
 }
